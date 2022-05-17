e8dfd85f5baa2b50b204269553d12b996e5c7788
Danilo Oliveira <dan.oliveira883@gmail.com>
2022-05-16 23:01:38 -0300

    style: changing global style.

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -5,17 +5,18 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    text-decoration: none;
   }
 
 
   body {
-    background: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.text};
+    /* background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text}; */
     font: 400 16px Roboto, sans-serif;
-  }
+  } 
 
-  a {
+  /* a {
     color: ${props => props.theme.colors.background};
     text-decoration: none;
-  }
+  } */
 `
