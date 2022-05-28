4e2322945731434cabbf1afa38594c96e0bcd4c8
Danilo Oliveira <dan.oliveira883@gmail.com>
2022-05-28 00:22:53 -0300

    feat: add new mode for export.

---
+++ src/@types/exports.ts
@@ -0,0 +1,6 @@
+import { ITypeProgramme } from '.'
+import { ITypeCard } from '.'
+import { ITypeData } from '.'
+import { ITypeHeader } from '.'
+
+export type { ITypeProgramme, ITypeCard, ITypeData, ITypeHeader }
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+import { Cards } from './Cards'
+import { Header } from './Header'
+
+export { Cards, Header }
