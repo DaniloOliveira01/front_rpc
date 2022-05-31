606316a0ec678eb31a13da774faf7e0583757af1
Danilo Oliveira <dan.oliveira883@gmail.com>
2022-05-31 00:19:35 -0300

    chore: add nextjs-progressbar.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,17 @@ import type { AppProps } from 'next/app'
 import GlobalStyles from '../styles/GlobalStyles'
 import { ThemeProvider } from 'styled-components'
 import theme from '../styles/theme'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <NextNProgress color="#02ff2c" startPosition={0.5} />
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
       <GlobalStyles />
     </ThemeProvider>
+    </>
   )
 }
 
